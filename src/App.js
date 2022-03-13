561963ca00a5e6416f5184b882339886e706dd91
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2022-03-12 20:44:11 -0500

    If apiKey changes get the latest hit count, added useRef so we can remove unwanted renders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import countapi from 'countapi-js';
 
 import Main from './components/main/main.component';
@@ -7,8 +7,23 @@ import Loading from './components/loading/loading.component';
 const defaultKey = '1ccb732e-b55a-4404-ad3f-0f99c02fe44e';
 
 const App = () => {
+  const isMounted = useRef(false);
   const [state, setState] = useState({ key: defaultKey, totalHits: undefined });
   const { key, totalHits } = state;
+
+  useEffect(() => {
+    const callGetAPI = () => countapi.get(key);
+    const getHandler = async () => {
+      let { value } = await callGetAPI();
+      value ||= 0;
+      
+      updateHits(value);
+    }
+
+    getHandler();
+
+    isMounted.current = true;
+  }, [key]);
 
   const updateHits = (hits) => {
     setState(prevState => ({ ...prevState, totalHits: hits }));
@@ -18,13 +33,15 @@ const App = () => {
     setState(prevState => ({ ...prevState, key: event.target.value }));
   }
 
-  return (
-    <Main
-      apiKey={key}
-      totalHits={totalHits}
-      inputHandler={inputHandler}
-      updateHits={updateHits}
+  return isMounted.current ? (
+    <Main 
+      apiKey={key} 
+      totalHits={totalHits} 
+      inputHandler={inputHandler} 
+      updateHits={updateHits} 
      />
+  ) : (
+    <Loading />
   )
 }
 
